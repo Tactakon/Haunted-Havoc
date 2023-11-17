035873035b45b4263b17b5daeb3837ac34d527ad
Tactakon <112525688+Tactakon@users.noreply.github.com>
2023-11-17 11:32:13 -0500

    added torches and mechanics, ghost dissapearance,

--- collision.js
+++ collision.js
@@ -1,4 +1,7 @@
-import { create, PlayerMovement } from './sprites.js';
+import { hasTorch, setHasTorch, distance, distanceThreshold, updateDistanceThreshold } from './main.js';
+import { create, ghosts, torches, PlayerMovement } from './sprites.js';
+import { checkTorchState, updateScreenBrightness } from './torches.js';
+
 
 var playerBlinking = false;
 var blinkDuration = 2000; // 2 seconds
@@ -6,29 +9,52 @@ var blinkInterval = 250; // Blink every 250ms
 var blinkCount = 0;
 var blinkIntervalId;
 
-function playerCollidesWithGhost(player, ghosts) {
-    if (!playerBlinking) {
-        console.log("hit");
-        playerBlinking = true;
-        blinkCount = 0;
-        player.visible = true; // Ensure the player is initially visible
+function playerCollidesWithTorch(player, torch) {
+    updateDistanceThreshold();
+    console.log("Threshold when you obtain a torch:", distanceThreshold);
+    // Handle the player obtaining a torch
+    console.log('Torch collected'); // Debugging line
+    setHasTorch(true, ghosts, distance);
+    const torchIndex = torches.indexOf(torch);
+    console.log('You got this torch:', torchIndex);
+    if (torchIndex !== -1) {
+        // Remove the torch from the array and destroy it
+        torches.splice(torchIndex, 1);
+        torch.destroy();
+    }
 
-        // Start the blinking interval
-        blinkIntervalId = setInterval(() => {
-            player.visible = !player.visible; // Toggle player visibility
-            blinkCount += blinkInterval;
-            
-            if (blinkCount >= blinkDuration) {
-                // Blinking duration is over
-                player.visible = true; // Ensure the player is fully visible
-                playerBlinking = false; // Reset the blinking flag
-                clearInterval(blinkIntervalId); // Clear the blinking interval
-            }
-        }, blinkInterval);
+    // Update the torch state and screen brightness
+    checkTorchState(player, distanceThreshold)
+
+    // Perform other actions when the player obtains a torch
+}
+
+
+function playerCollidesWithGhost(player, ghosts) {
+    if (!hasTorch) {
+        if (!playerBlinking) {
+            console.log("hit");
+            playerBlinking = true;
+            blinkCount = 0;
+            player.visible = true; // Ensure the player is initially visible
+
+            // Start the blinking interval
+            blinkIntervalId = setInterval(() => {
+                player.visible = !player.visible; // Toggle player visibility
+                blinkCount += blinkInterval;
+                
+                if (blinkCount >= blinkDuration) {
+                    // Blinking duration is over
+                    player.visible = true; // Ensure the player is fully visible
+                    playerBlinking = false; // Reset the blinking flag
+                    clearInterval(blinkIntervalId); // Clear the blinking interval
+                }
+            }, blinkInterval);
+        }
     }
 }
 
-export { playerCollidesWithGhost };
+export { playerCollidesWithTorch, playerCollidesWithGhost };
 
 
 
--- sprites.js
+++ sprites.js
@@ -1,19 +1,21 @@
-import { playerCollidesWithGhost } from './collision.js';
+import { hasTorch, setHasTorch, distance, distanceThreshold, updateDistanceThreshold } from './main.js';
+import { playerCollidesWithTorch, playerCollidesWithGhost } from './collision.js';
+import { checkTorchState, updateScreenBrightness } from './torches.js';
 
 var player;
 var cursors;
 var playerSpeed = 200; 
 var playerBlinking = false;
-
+var ghosts;
+var torches = [];
 function create (scene)
 {
-
     // Create the player sprite (remove the 'var' keyword)
     player = scene.physics.add.sprite(100, 550, 'player');
     player.setDisplaySize(100, 100);
     player.setCollideWorldBounds(true); // Prevent the player from going off-screen
 
-    var ghosts = scene.physics.add.group();
+    ghosts = scene.physics.add.group();
     // Define the path for the ghost to follow
     var path = scene.add.path(100, 100); 
     path.lineTo(300, 100); 
@@ -62,9 +64,23 @@ function create (scene)
         yoyo: true, // Make the ghost move back and forth on the path
         repeat: -1, // -1 means repeat indefinitely
     });
+    ghosts.children.iterate(function (ghost) {
+        ghost.setVisible(!hasTorch);
+    });
+    const torch1 = scene.physics.add.sprite(800, 600, 'torch');
+    const torch2 = scene.physics.add.sprite(500, 100, 'torch');
+    const torch3 = scene.physics.add.sprite(200, 300, 'torch');
 
+    torch1.setDisplaySize(30, 30);
+    torch2.setDisplaySize(30, 30);
+    torch3.setDisplaySize(30, 30);
+
+    torches.push(torch1, torch2, torch3);
+    checkTorchState(player, distanceThreshold);
+    console.log("Initial Threshold:", distanceThreshold);
     // Set up keyboard input
     cursors = scene.input.keyboard.createCursorKeys();
+    scene.physics.add.overlap(player, torches, playerCollidesWithTorch, null, scene);
     scene.physics.add.overlap(player, ghosts.getChildren(), playerCollidesWithGhost, null, scene);
 }
 
@@ -76,15 +92,19 @@ function PlayerMovement(scene) {
     // Check for keyboard input to move the player
     if (cursors.left.isDown) {
         velocityX = -playerSpeed; // Move left
+        checkTorchState(player, distanceThreshold);
     }
     if (cursors.right.isDown) {
         velocityX = playerSpeed; // Move right
+        checkTorchState(player, distanceThreshold);
     }
     if (cursors.up.isDown) {
         velocityY = -playerSpeed; // Move up
+        checkTorchState(player, distanceThreshold);
     }
     if (cursors.down.isDown) {
         velocityY = playerSpeed; // Move down
+        checkTorchState(player, distanceThreshold);
     }
 
     // Normalize the diagonal movement
@@ -103,4 +123,4 @@ function PlayerMovement(scene) {
     }
 }
 
-export { create, PlayerMovement };
+export { create, ghosts, torches, PlayerMovement };
